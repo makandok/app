0fb941b650de529d56396c807d191ebbae83536d
Zed Coder <makandok@users.noreply.github.com>
2025-07-08 19:31:46 +0000

    refinements to index

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import './bootstrap.min.css';
 
-
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './jquery.min.js';
-import './bootstrap.min.js';
-import './powerbi.min.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
